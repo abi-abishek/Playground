de31ff077629d2efb656cc995c2793592c42ffc4
ABISHEK E <plrabiyt@gmail.com>
2024-02-26 16:43:19 +0530

    Practical 2A

--- Java/Firstprogram.java
+++ Java/Firstprogram.java
@@ -5,9 +5,18 @@ public class Firstprogram{
         char data = 'A';
         boolean bool = true;
 
+        byte myNum = 80; //byte -128 to 127
+        short myshort =8000;
+        long mylong = 11550000000000L;
+        double myDouble = 75.59d;
+
         System.out.println(num); 
         System.out.println(num1); 
         System.out.println(data); 
         System.out.println(bool); 
+        System.out.println(myNum); 
+        System.out.println(myshort); 
+        System.out.println(mylong); 
+        System.out.println(myDouble); 
     }
 }
