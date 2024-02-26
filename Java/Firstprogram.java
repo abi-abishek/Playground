1a7264c003d7183ff3278cd9f9a1cac811098193
ABISHEK E <plrabiyt@gmail.com>
2024-02-26 16:34:07 +0530

    Practical 1

--- Java/Firstprogram.java
+++ Java/Firstprogram.java
@@ -1,5 +1,13 @@
 public class Firstprogram{
     public static void main(String[] args){
-        System.out.println("Hello World!"); 
+        int num = 50;
+        float num1 = 3.14f;
+        char data = 'A';
+        boolean bool = true;
+
+        System.out.println(num); 
+        System.out.println(num1); 
+        System.out.println(data); 
+        System.out.println(bool); 
     }
 }
