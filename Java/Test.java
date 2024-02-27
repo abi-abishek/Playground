2d01b0dc66d9e4126e1f46fec394047ff068b78e
ABISHEK E <plrabiyt@gmail.com>
2024-02-27 12:58:02 +0530

    Operators test

---
+++ Java/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        int x = 5;
+        x += 2*3;
+        System.out.println(x);
+
+        int a = 5;
+        int b = 7;
+        int c = a++ + ++b;
+        System.out.println(c);
+    }
+}
