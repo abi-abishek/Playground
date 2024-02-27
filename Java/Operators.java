bef1a0224bffd4219da0c8a8fd3b917a0f3950eb
ABISHEK E <plrabiyt@gmail.com>
2024-02-27 12:17:40 +0530

    Operators diff

--- Java/Operators.java
+++ Java/Operators.java
@@ -4,5 +4,10 @@ public class Operators {
         int num2 = 10;
         int sum = num1 + num2;
         System.out.println(sum);
+
+        int num3 = 10;
+        int num4 = 5;
+        int diff = num3 - num4;
+        System.out.println(diff);
     }
 }
