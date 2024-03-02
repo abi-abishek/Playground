c53812c0a45b019c5a6c826056884951df55c3e2
ABISHEK E <plrabiyt@gmail.com>
2024-03-02 16:56:18 +0530

    If...Else Odd or Even

--- Java/ifelse.java
+++ Java/ifelse.java
@@ -1,12 +1,11 @@
 public class ifelse {
     public static void main(String[] args) {
-        int num =5;
-        int num1 = 3;
-        if(num < num1){
-            System.out.println("num is less than num1");
+        int num =3;
+        if(num % 2 == 0){
+            System.out.println("Number is even");
         }
         else{
-            System.out.println("num1 is greater than num");
+            System.out.println("Number is odd");
         }
     }
 }
