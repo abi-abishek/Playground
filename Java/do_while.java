15b889d623ba2ceb93e6c3114b9909cb463ff578
ABISHEK E <plrabiyt@gmail.com>
2024-03-02 23:02:54 +0530

    Do while Practical

--- Java/do_while.java
+++ Java/do_while.java
@@ -1,8 +1,9 @@
 public class do_while {
     public static void main(String[] args) {
+        int num = 9;
         int i = 1;
-        while (i<6){
-            System.out.print(i);
+        while (i<=10){
+            System.out.println(num+" x "+i+" = "+num*i);
             i++;
         }
     }
