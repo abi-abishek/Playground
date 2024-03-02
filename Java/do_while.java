1407599c5bfcb4aae4be04bb86493071d3c030b9
ABISHEK E <plrabiyt@gmail.com>
2024-03-02 18:05:52 +0530

    Do while A

---
+++ Java/do_while.java
@@ -0,0 +1,9 @@
+public class do_while {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i<6){
+            System.out.print(i);
+            i++;
+        }
+    }
+}
