bafd5de811b385d7b07f5ccfa51d2eea5f8c4f18
ABISHEK E <plrabiyt@gmail.com>
2024-03-02 16:54:22 +0530

    If...Else Practical

---
+++ Java/ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int num =5;
+        int num1 = 3;
+        if(num < num1){
+            System.out.println("num is less than num1");
+        }
+        else{
+            System.out.println("num1 is greater than num");
+        }
+    }
+}
