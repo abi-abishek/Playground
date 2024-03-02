95e2e2391e2331372052207da1c632c5e9275468
ABISHEK E <plrabiyt@gmail.com>
2024-03-02 23:06:01 +0530

    Do while Test

--- Java/Test.java
+++ Java/Test.java
@@ -1,14 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        int a= 2+3<<5/6;
-        int b=6>>4%4;
-        if(a>b)
-        {
-        System.out.println(a+6);
-        }
-        else
-        {
-        System.out.println(b-3);
-        }
+        int a=1;
+while(a<4)
+{
+  System.out.print(a + " ");
+  a++;
+}
     }
 }
