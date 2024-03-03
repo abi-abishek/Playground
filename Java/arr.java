a8384e30c0b26f61663574e38f0b015f0d3cee4a
ABISHEK E <plrabiyt@gmail.com>
2024-03-03 13:44:09 +0530

    1-D Array Practical-2A

--- Java/arr.java
+++ Java/arr.java
@@ -2,8 +2,8 @@ public class arr {
     public static void main(String[] args) {
         String[] cars = {"Tesla","BMW","Ford"};
         int[] numbers = {10,20,30,40};
-        for(int i = 0; i < cars.length; i++){
-            System.out.println(cars[i]);
+        for(String i : cars){
+            System.out.println(i);
         }
         for(int i = 0; i < numbers.length; i++){
             System.out.println(numbers[i]);
